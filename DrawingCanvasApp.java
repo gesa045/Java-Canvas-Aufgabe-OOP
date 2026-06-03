44a7c28fa8928bc920aaee90b8aa1a30a011b441
Gesa <gm.grabler@gmail.com>
2026-06-03 17:27:07 +0200

    Add menübalken und ersten Test Button

--- DrawingCanvasApp.java
+++ DrawingCanvasApp.java
@@ -1,7 +1,11 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Color;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
 
 /**
  * A basic Swing drawing application with only a freehand drawing canvas.
@@ -17,11 +21,24 @@ public class DrawingCanvasApp extends JFrame {
         super("Drawing Canvas App");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        canvas = new DrawingCanvas();
-
         setLayout(new BorderLayout());
+
+        // Canvas in der Mitte
+        canvas = new DrawingCanvas();
         add(canvas, BorderLayout.CENTER);
 
+
+        // MenuBalken oben für zukünftige Buttons
+        JPanel toolbar = new JPanel();
+        add(toolbar, BorderLayout.NORTH);
+        toolbar.setBackground(Color.LIGHT_GRAY);
+
+        // erster Button zum Testen
+        JButton ClearButton = new JButton("Test Button");
+        toolbar.add(ClearButton);
+
+
+        // Setze minimale Fenstergröße und initiale Größe
         setMinimumSize(new Dimension(900, 650));
         setSize(900, 650);
     }
